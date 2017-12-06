de2e66964a0110e4228a3135131e5c9b266a7fe9
acnaman <acnadin01@gmail.com>
2017-12-07 01:55:57 +0900

    change func name

--- main.c
+++ main.c
@@ -30,19 +30,19 @@ int main(void)
     for(i = COUNT_UP; i < list_size; i+=COUNT_UP){
         /* 都度確保 */
         c_start = clock();
-        littlesize(i);
+        allocate_by_time(i);
         c_end = clock();
         res1 = (int)c_end - c_start;
 
         /* 一挙確保 */
         c_start = clock();
-        bigmemory(i);
+        allocate_bigmemory(i);
         c_end = clock();
         res2 = (int)c_end - c_start;
         
         /* 折衷案 */
         c_start = clock();
-        hundredtimes(i);
+        allocate_bigmemory(i);
         c_end = clock();
         res3 = (int)c_end - c_start;
 
@@ -54,49 +54,55 @@ int main(void)
 }
 
 /* ループのたびにreallocし直す */
-void littlesize(int size){
+void allocate_by_time(int size){
     int i;
     const int sz = size;
     STTEST *ptstruct;
     
     for(i = 0; i < sz ;i++){
         if(i == 0) {
+            /* 初回のみmalloc */
             ptstruct = (STTEST*)malloc(sizeof(STTEST));
         } else {
             ptstruct = (STTEST*)realloc(ptstruct, sizeof(STTEST)*(i + 1));
         }
+        /* 適当な処理 */
         ptstruct[i].str[0] = '/0';
     }
     free(ptstruct);
 }
 
-/* とにかく最初に大きなメモリサイズを取得する */
-void bigmemory(int size){
+/* 最初に十分なメモリサイズを取得する */
+void allocate_bigmemory(int size){
     int i;
     const int sz = size;
     const int BIGSIZE = LIST_SIZE;
     STTEST *ptstruct;
 
+    /* 最大要素数分のメモリを確保 */
     ptstruct = (STTEST*)malloc(sizeof(STTEST) * BIGSIZE);
     
     for(i = 0; i < sz ;i++){
+        /* 適当な処理 */
         ptstruct[i].str[0] = '/0';
     }
     free(ptstruct);
 }
 
 /* 一定数ずつ確保する */
-void hundredtimes(int size){
+void allocate_by_span(int size){
     int i;
     const int sz = size;
     STTEST *ptstruct;
     
     for(i = 0; i < sz ;i++){
         if(i == 0) {
+            /* 初回のみmalloc */
             ptstruct = (STTEST*)malloc(sizeof(STTEST)*ALLOC_COUNT);
         } else if (i % ALLOC_COUNT == 0){
             ptstruct = (STTEST*)realloc(ptstruct, sizeof(STTEST)*(i + ALLOC_COUNT));
         }
+        /* 適当な処理 */
         ptstruct[i].str[0] = '/0';
     }
     free(ptstruct);
